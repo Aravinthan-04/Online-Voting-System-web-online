120437feffbf8b35e19885cc991f17b3d8f4967d
Aravinthan-04 <mr.donaravinthanloverwithfuture@gmail.com>
2025-04-05 23:43:07 +0530

    Update setup_db.py

--- setup_db.py
+++ setup_db.py
@@ -6,12 +6,17 @@ with app.app_context():
 
     # Add sample voters
     sample_voters = [
-        
+        Voter(voter_id="aravinth", password="2005"),
+        Voter(voter_id="mujibu", password="2004"),
+        Voter(voter_id="amizhthan", password="2005"),
+        Voter(voter_id="kishore", password="2002"),
     ]
 
     # Add candidates
     sample_candidates = [
-        
+        Candidate(name="REPRESENTATIVE"),
+        Candidate(name="ASSISTENT REPRESENTATIVE"),
+    
     ]
 
     db.session.bulk_save_objects(sample_voters)
